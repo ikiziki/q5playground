b1efe9648be3d322b1686582b0bb692e07d3b900
Kizik <cgeese@gmail.com>
2026-08-14 20:12:32 -0400

    Fix spacing project wide

--- projects/quadtree/src/main.js
+++ projects/quadtree/src/main.js
@@ -2,34 +2,34 @@ let theme;
 let qt;
 
 function setup() {
-    theme = new ThemeEngine();
-		qt = new QuadTree(0, 0, windowWidth, windowHeight, 10, 0, 4);
-    createCanvas(windowWidth, windowHeight);
+	theme = new ThemeEngine();
+	qt = new QuadTree(0, 0, windowWidth, windowHeight, 10, 0, 4);
+	createCanvas(windowWidth, windowHeight);
 		
-		console.log(qt)
+	console.log(qt)
 }
 
 function update(deltaTime) {
 }
 
 function draw() {
-    background(theme.bg);
+	background(theme.bg);
 		
-		push();
-		fill(theme.fg);
-		if (touches.length > 0) {
-        ellipse(touches[0].x, touches[0].y, 25, 25);
-				console.log(floor(touches[0].x), floor(touches[0].y));
-    }
-		pop();
+	push();
+	fill(theme.fg);
+	if (touches.length > 0) {
+  	ellipse(touches[0].x, touches[0].y, 25, 25);
+		console.log(floor(touches[0].x), floor(touches[0].y));
+  }
+	pop();
 		
-		qt.subdivide()
+	qt.subdivide()
 		
-		push()
-		qt.draw();
-		pop();
+	push()
+	qt.draw();
+	pop();
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight);
 }
