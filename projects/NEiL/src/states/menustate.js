89f2fd06a4ea634673e6172165ceaf9cd9f8f36f
Darth Vader <Chris.Geese@iCloud.com>
2026-08-27 13:56:06 -0400

    updates

--- projects/NEiL/src/states/menustate.js
+++ projects/NEiL/src/states/menustate.js
@@ -4,21 +4,17 @@
 class MenuState extends BaseState {
 	constructor() {
 		super();
-		this.sf = null
 	}
 
 	enter() {
 		console.log("menu loaded")
-		this.sf = new StarField();
 	}
 
 	update(deltaTime) {
-		this.sf.update(deltaTime)
 	}
 
 	draw() {
 		background(theme.bg)
-		this.sf.draw();
 	}
 
 	touchStarted(touches) {}
