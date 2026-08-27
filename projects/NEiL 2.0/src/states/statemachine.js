9b583d0b72c307b210025201219c3fe8c4a507b5
kizik <cgeese@gmail.com>
2026-08-27 10:23:23 -0400

    input handling

--- projects/NEiL 2.0/src/states/statemachine.js
+++ projects/NEiL 2.0/src/states/statemachine.js
@@ -1,15 +1,72 @@
 class StateMachine {
-    constructor() {}
+    constructor() {
+        this.currentState = null;
+        this.previousState = null;
+        this.stateStack = [];
+    }
 
-    enter(state) {}
+    enter(state) {
+        if (this.currentState && this.currentState.exit) {
+            this.currentState.exit();
+        }
+        this.previousState = this.currentState;
+        this.currentState = state;
+        if (this.currentState && this.currentState.enter) {
+            this.currentState.enter();
+        }
+    }
 
-    pause() {}
+    pause(state) {
+        if (!this.currentState || !state) {
+            return;
+        }
+        if (this.currentState.pause) {
+            this.currentState.pause();
+        }
+        this.previousState = this.currentState;
+        this.stateStack.push(this.currentState);
+        this.currentState = state;
+        if (this.currentState.enter) {
+            this.currentState.enter();
+        }
+    }
 
-    resume() {}
+    resume() {
+        if (this.stateStack.length === 0) {
+            return;
+        }
+        if (this.currentState && this.currentState.exit) {
+            this.currentState.exit();
+        }
+        this.previousState = this.currentState;
+        this.currentState = this.stateStack.pop();
+        if (this.currentState.resume) {
+            this.currentState.resume();
+        }
+    }
 
-    update(deltaTime) {}
+    handleInput(inputName, event) {
+        if (!this.currentState || !this.currentState[inputName]) {
+            return true;
+        }
+        return this.currentState[inputName].call(this.currentState, event);
+    }
 
-    draw() {}
+    update(deltaTime) {
+        if (this.currentState && this.currentState.update) {
+            this.currentState.update(deltaTime);
+        }
+    }
 
-    exit() {}
+    draw() { 
+        if (this.currentState && this.currentState.draw) {
+            this.currentState.draw();
+        }
+    }
+
+    exit() {
+        if (this.currentState && this.currentState.exit) {
+            this.currentState.exit();
+        }
+    }
 }
