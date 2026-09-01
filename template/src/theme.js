ebee653724bc595a73c5a145256efca08ee1efd2
Darth Vader <Chris.Geese@iCloud.com>
2026-09-01 19:25:31 -0400

    updates

--- template/src/theme.js
+++ template/src/theme.js
@@ -4,37 +4,37 @@
 
 class ThemeEngine {
 
-    constructor() {
-        this.mediaQuery = window.matchMedia(
-            "(prefers-color-scheme: dark)"
-        );
+	constructor() {
+		this.mediaQuery = window.matchMedia(
+			"(prefers-color-scheme: dark)"
+		);
 
-        this.update();
+		this.update();
 
-        // Listen for system theme changes
-        this.mediaQuery.addEventListener(
-            "change",
-            () => this.update()
-        );
-    }
+		// Listen for system theme changes
+		this.mediaQuery.addEventListener(
+			"change",
+			() => this.update()
+		);
+	}
 
-    // Detect the current system appearance
-    getSystemStyle() {
-        return this.mediaQuery.matches
-            ? "dark"
-            : "light";
-    }
+	// Detect the current system appearance
+	getSystemStyle() {
+		return this.mediaQuery.matches ?
+			"dark" :
+			"light";
+	}
 
-    // Update theme values
-    update() {
-        this.style = this.getSystemStyle();
+	// Update theme values
+	update() {
+		this.style = this.getSystemStyle();
 
-        if (this.style === "dark") {
-            this.bg = [35, 35, 35];
-            this.fg = [235, 235, 235];
-        } else {
-            this.bg = [235, 235, 235];
-            this.fg = [35, 35, 35];
-        }
-    }
+		if (this.style === "dark") {
+			this.bg = [35, 35, 35];
+			this.fg = [235, 235, 235];
+		} else {
+			this.bg = [235, 235, 235];
+			this.fg = [35, 35, 35];
+		}
+	}
 }
